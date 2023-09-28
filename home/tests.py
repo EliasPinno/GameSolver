121f22a3a94e71cda33609fd577acfafd816d923
EliasPinno <pucas946@gmail.com>
2023-09-28 14:18:04 -0600

    Created an initial test which should run on main.

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+class YourTestClass(TestCase):
+
+    def setUpTestData(self):
+        print("Ran setUpTestData. Should only run once")
+
+    def setUp(self):
+        print("Ran test setup: should run before each test")
+        pass
+
+    def test_true_is_true(self):
+        self.assertTrue(True == True)
+        pass
