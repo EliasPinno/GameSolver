bf284c514e8d89701bda1f7b2b73141991e8af3f
EliasPinno <pucas946@gmail.com>
2023-09-30 14:21:32 -0600

    Added peg solitaire to routing.

--- game_solver/urls.py
+++ game_solver/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
+    path("peg-solitaire", include("peg_solitaire.urls"))
 ]
