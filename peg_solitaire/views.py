ec6dd34b16133fb15204908e75dc5e7809547525
EliasPinno <pucas946@gmail.com>
2023-09-30 18:38:00 -0600

    Added the ability to output a solution set for grid solitaire.

--- peg_solitaire/views.py
+++ peg_solitaire/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.template.loader import render_to_string
+from render_board import GridSolitaireUtilities as gs
 
 # Create your views here.
 
 
 def solution_view(request):
-    boards = [[[1, 2], [3, 4], [5, 6]], [[1, 2], [3, 4]]]
+    solution = gs.getSolution()
+    boards = gs.getAllBoardsInSolution(gs.startingBoard, solution)
+    #boards = [[[1, 2], [3, 4], [5, 6]], [[1, 2], [3, 4]]]
     return render(request, "pegSolitaire.html", {"boards": boards})
