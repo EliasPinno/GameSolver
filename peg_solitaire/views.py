93c1c0b2a587f4c1ca1045308ee40b959585685a
EliasPinno <pucas946@gmail.com>
2023-09-30 18:23:32 -0600

    Ran black linter.

--- peg_solitaire/views.py
+++ peg_solitaire/views.py
@@ -3,6 +3,7 @@ from django.template.loader import render_to_string
 
 # Create your views here.
 
+
 def solution_view(request):
-    boards = [[[1,2],[3,4],[5,6]], [[1,2],[3,4]]]
+    boards = [[[1, 2], [3, 4], [5, 6]], [[1, 2], [3, 4]]]
     return render(request, "pegSolitaire.html", {"boards": boards})
