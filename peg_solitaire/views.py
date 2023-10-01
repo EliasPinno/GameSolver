491f20f9e43df0b29915adb8bf5b428f36d5bce7
EliasPinno <pucas946@gmail.com>
2023-09-30 18:45:08 -0600

    Ran black lint.

--- peg_solitaire/views.py
+++ peg_solitaire/views.py
@@ -8,5 +8,5 @@ from render_board import GridSolitaireUtilities as gs
 def solution_view(request):
     solution = gs.getSolution()
     boards = gs.getAllBoardsInSolution(gs.startingBoard, solution)
-    #boards = [[[1, 2], [3, 4], [5, 6]], [[1, 2], [3, 4]]]
+    # boards = [[[1, 2], [3, 4], [5, 6]], [[1, 2], [3, 4]]]
     return render(request, "pegSolitaire.html", {"boards": boards})
