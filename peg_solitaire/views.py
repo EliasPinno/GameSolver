af63a908601c648b027dc2fbd3b2e2a24adef183
EliasPinno <pucas946@gmail.com>
2023-09-30 18:10:16 -0600

    Did some more misc maintenance.

--- peg_solitaire/views.py
+++ peg_solitaire/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
 # Create your views here.
 
 def solution_view(request):
-    return render(request, "pegSolitaire.html")
+    boards = [[[1,2],[3,4],[5,6]], [[1,2],[3,4]]]
+    return render(request, "pegSolitaire.html", {"boards": boards})
